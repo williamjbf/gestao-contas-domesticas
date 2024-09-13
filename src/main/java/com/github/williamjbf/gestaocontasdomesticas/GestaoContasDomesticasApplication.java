b94454b988cca6b975eb06ba23921cad1e66f904
williamjbf <williamjbf@gmail.com>
2024-09-13 18:37:30 -0300

    feat: Configuracao inicial do projeto

---
+++ src/main/java/com/github/williamjbf/gestaocontasdomesticas/GestaoContasDomesticasApplication.java
@@ -0,0 +1,13 @@
+package com.github.williamjbf.gestaocontasdomesticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoContasDomesticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoContasDomesticasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/williamjbf/gestaocontasdomesticas/GestaoContasDomesticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.williamjbf.gestaocontasdomesticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoContasDomesticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

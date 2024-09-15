5e99847d590f5474c7d6e573d967845d898b95bf
williamjbf <williamjbf@gmail.com>
2024-09-15 18:57:05 -0300

    feat(BackEnd): Implementado HandlerException para exibicao de erros

---
+++ backend/src/main/java/com/github/williamjbf/gestaocontasdomesticas/infraestrutura/handler/CustomExceptionHandler.java
@@ -0,0 +1,61 @@
+package com.github.williamjbf.gestaocontasdomesticas.infraestrutura.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.transaction.TransactionSystemException;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+
+import java.io.IOException;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {AccessDeniedException.class})
+    public void commence(final HttpServletRequest request,
+                         final HttpServletResponse response,
+                         final AccessDeniedException ex) throws IOException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getOutputStream().println("{ \"error\": \"" + ex.getMessage() + "\" }");
+    }
+
+    @ExceptionHandler({
+            MultipartException.class,
+            MissingServletRequestPartException.class,
+            MissingServletRequestParameterException.class,
+            MethodArgumentTypeMismatchException.class,
+            ConstraintViolationException.class,
+            TransactionSystemException.class,
+            HttpRequestMethodNotSupportedException.class,
+            HttpMediaTypeNotSupportedException.class,
+            DataIntegrityViolationException.class,
+            EmptyResultDataAccessException.class,
+            JpaObjectRetrievalFailureException.class,
+            TypeMismatchException.class,
+            BindException.class,
+            MethodArgumentNotValidException.class
+    })
+    public ResponseEntity<Object> handleGenericException(final Exception ex) {
+        logger.error(ex.getStackTrace());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getLocalizedMessage());
+    }
+
+}
